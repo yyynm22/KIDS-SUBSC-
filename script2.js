cca7f682f72a7fdc2d6c49590b450bfcfaa8ad10
yyynm22 <yminagawa@dl-techx-xedge.net>
2024-09-06 16:48:24 +0900

    資源のデプロイ

--- script2.js
+++ script2.js
@@ -5,12 +5,17 @@ new Vue({
       return {
           tab: 0, // 初期タブ
           dataList: [], // 会員登録情報を格納するリスト
+        user_id: sessionStorage.getItem('user_id'), // ログインセッションからユーザーIDを取得
       };
   },
   methods: {
       async fetchData() {
           try {
-              const response = await axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT');
+              const response = await axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT', {
+                  params: {
+                      user_id: this.user_id, // ここでユーザーIDを送信
+                  }
+              });
               // 結果をdataListに格納
               this.dataList = response.data;
               console.log(this.dataList);
@@ -19,8 +24,8 @@ new Vue({
           }
       },
       addData() {
-          // 商品の検索画面に遷移
-          window.location.href = '/index1.html';
+          // Googleの検索画面に遷移
+          window.location.href = 'https://www.google.com';
       },
   },
   mounted() {
