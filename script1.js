257ed86fd08e8d31aaf36d151bb7479763dcd57d
yyynm22 <yminagawa@dl-techx-xedge.net>
2024-09-06 15:23:16 +0900

    資源のデプロイ

--- script1.js
+++ script1.js
@@ -17,12 +17,12 @@ const app = new Vue({
     methods: {
       mypage() {
         // マイページ遷移を実行
-  window.location.href = '';
+  window.location.href = '/index2.html';
         // ボタンのクリックイベントを処理する関数（必要に応じて追加）
       },
       Logout() {
         // ログアウトページ遷移を実行
-  window.location.href = '';
+  window.location.href = '/index.html';
         // ボタンのクリックイベントを処理する関数（必要に応じて追加）
       },
       readData1: async function () {
---
+++ script2.js
@@ -0,0 +1,30 @@
+new Vue({
+  el: '#app',
+  vuetify: new Vuetify(),
+  data() {
+      return {
+          tab: 0, // 初期タブ
+          dataList: [], // 会員登録情報を格納するリスト
+      };
+  },
+  methods: {
+      async fetchData() {
+          try {
+              const response = await axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT');
+              // 結果をdataListに格納
+              this.dataList = response.data;
+              console.log(this.dataList);
+          } catch (error) {
+              console.error("APIの取得に失敗しました", error);
+          }
+      },
+      addData() {
+          // 商品の検索画面に遷移
+          window.location.href = '/index1.html';
+      },
+  },
+  mounted() {
+      // マウント時にデータを取得
+      this.fetchData();
+  },
+});
