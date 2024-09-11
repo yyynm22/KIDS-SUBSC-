61186508530f069f0eb6c5d64bf87eb786401c84
aoiwhite7 <afurugoori@dl-techx-xedge.net>
2024-09-11 13:54:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -68,3 +68,73 @@ const app = new Vue({
         }
     }
   });
+  
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const tabs = document.getElementsByClassName('tab');
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].addEventListener('click', tabSwitch, false);
+    }
+  
+    function tabSwitch() {
+        document.getElementsByClassName('is-active')[0].classList.remove('is-active');
+        this.classList.add('is-active');
+        document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+        const arrayTabs = Array.prototype.slice.call(tabs);
+        const index = arrayTabs.indexOf(this);
+        document.getElementsByClassName('panel')[index].classList.add('is-show');
+    };
+  }, false);
+  
+  const slide = document.getElementById('slide');
+  const prev = document.getElementById('prev');
+  const next = document.getElementById('next');
+  const indicator = document.getElementById('indicator');
+  const lists = document.querySelectorAll('.list');
+  const totalSlides = lists.length;
+  let count = 0;
+  let autoPlayInterval;
+  function updateListBackground() {
+  for (let i = 0; i < lists.length; i++) {
+    lists[i].style.backgroundColor = i === count % totalSlides ? '#000' : '#fff';
+  }
+  }
+  function nextClick() {
+  slide.classList.remove(`slide${count % totalSlides + 1}`);
+  count++;
+  slide.classList.add(`slide${count % totalSlides + 1}`);
+  updateListBackground();
+  }
+  function prevClick() {
+  slide.classList.remove(`slide${count % totalSlides + 1}`);
+  count--;
+  if (count < 0) count = totalSlides - 1;
+  slide.classList.add(`slide${count % totalSlides + 1}`);
+  updateListBackground();
+  }
+  function startAutoPlay() {
+  autoPlayInterval = setInterval(nextClick, 3000);
+  }
+  function resetAutoPlayInterval() {
+  clearInterval(autoPlayInterval);
+  startAutoPlay();
+  }
+  next.addEventListener('click', () => {
+  nextClick();
+  resetAutoPlayInterval();
+  });
+  prev.addEventListener('click', () => {
+  prevClick();
+  resetAutoPlayInterval();
+  });
+  indicator.addEventListener('click', (event) => {
+  if (event.target.classList.contains('list')) {
+    const index = Array.from(lists).indexOf(event.target);
+    slide.classList.remove(`slide${count % totalSlides + 1}`);
+    count = index;
+    slide.classList.add(`slide${count % totalSlides + 1}`);
+    updateListBackground();
+    resetAutoPlayInterval();
+  }
+  });
+  startAutoPlay();
