25beccdd613cfc98ea4b64ccb283715ee70a5537
aoiwhite7 <afurugoori@dl-techx-xedge.net>
2024-09-12 13:31:47 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -34,52 +34,54 @@ new Vue({
             window.location.href = '/index1.html';
         },
         fetchOrderHistory() {
-            // SELECT6の呼び出し
             axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT6')
                 .then(select6Response => {
                     const orderList = select6Response.data.List;
                     const orderIds = orderList.map(order => order.order_id);
-
+        
                     // 各order_idに対してSELECT4を呼び出す
                     const select4Promises = orderIds.map(order_id =>
                         axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT4', {
                             params: { order_id }
                         })
                     );
-
+        
                     return Promise.all(select4Promises);
                 })
                 .then(select4Responses => {
                     const orderDetails = [];
-
+        
                     select4Responses.forEach((response, index) => {
                         const order_id = response.config.params.order_id;
-                        const cartItems = response.data.List;
-
-                        // 各商品ごとにSELECT3を呼び出して商品情報を取得
+                        const cartItems = response.data.List || [];  // cartItemsが存在しない場合は空配列を使用
+        
+                        let totalQuantity = 0;
+                        cartItems.forEach(item => {
+                            totalQuantity += item.quantity;
+                        });
+        
+                        orderDetails.push({
+                            order_id,
+                            total_quantity: totalQuantity,
+                            cartItems
+                        });
+        
                         const productPromises = cartItems.map(item =>
                             axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT3', {
                                 params: { product_id: item.product_id }
                             })
                         );
-
-                        orderDetails.push({
-                            order_id,
-                            cartItems
-                        });
-
+        
                         return Promise.all(productPromises).then(productResponses => {
-                            productResponses.forEach((productResponse, index) => {
-                                const productInfo = productResponse.data;
-                                orderDetails[index].cartItems[index].productInfo = productInfo;
+                            productResponses.forEach((productResponse, i) => {
+                                cartItems[i].productInfo = productResponse.data;
                             });
                         });
                     });
-
+        
                     return orderDetails;
                 })
                 .then(orderHistory => {
-                    // フロントエンドで取得したデータを使用して注文履歴を更新
                     this.orderHistory = orderHistory;
                     console.log('Order History:', this.orderHistory);
                 })
@@ -87,6 +89,7 @@ new Vue({
                     console.error('注文履歴の取得に失敗しました:', error);
                 });
         },
+        
         Logout() {
             window.location.href = '/index.html';
         }
