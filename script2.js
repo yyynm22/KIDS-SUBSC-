dc21d699b39f3009bd1f7c4e30c8214482392f0b
aoiwhite7 <afurugoori@dl-techx-xedge.net>
2024-09-12 15:32:57 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -32,67 +32,71 @@ new Vue({
         addData() {
             window.location.href = '/index1.html';
         },
-        fetchOrderHistory() {
-            const userId = this.userData.user_id; // ログインユーザーのIDを取得
-            
-            axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT6')
-                .then(select6Response => {
-                    const orderList = select6Response.data.List;
-                    const orderIds = orderList.map(order => order.order_id);
+fetchOrderHistory() {
+    const userId = this.userData.user_id; // ログインユーザーのIDを取得
 
-                    // 各order_idに対してSELECT4を呼び出す
-                    const select4Promises = orderIds.map(order_id =>
-                        axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT4', {
-                            params: { order_id, user_id: userId } // ユーザーIDをパラメーターとして追加
-                        })
-                    );
+    // ユーザーIDに基づく注文履歴を取得するAPIエンドポイント
+    axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT4', {
+        params: { user_id: userId } // ユーザーIDをパラメーターとして追加
+    })
+    .then(select4Response => {
+        const orderList = select4Response.data.List;
 
-                    return Promise.all(select4Promises).then(select4Responses => {
-                        const orderDetails = select4Responses.map((response, index) => {
-                            const order_id = orderIds[index];
-                            const cartItems = response.data.List || []; // カートのアイテムがない場合は空配列
+        // 各order_idに対してSELECT6を呼び出す
+        const orderIds = [...new Set(orderList.map(order => order.order_id))]; // 重複を除いた注文IDのリスト
 
-                            let totalQuantity = 0;
-                            cartItems.forEach(item => {
-                                totalQuantity += item.quantity;
-                            });
+        const select6Promises = orderIds.map(order_id =>
+            axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT6', {
+                params: { order_id } // 注文IDをパラメーターとして追加
+            })
+        );
 
-                            // 各カートアイテムに対応する商品情報を取得する
-                            const productPromises = cartItems.map(item =>
-                                axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT3', {
-                                    params: { product_id: item.product_id }
-                                }).then(productResponse => {
-                                    const productData = productResponse.data.List[0]; // 商品情報の1件目を使用
-                                    return {
-                                        ...item,
-                                        product_name: productData.product_name,
-                                        product_category: productData.product_category,
-                                        product_gender: productData.product_gender,
-                                        product_image_url: productData.URL
-                                    };
-                                })
-                            );
+        return Promise.all(select6Promises).then(select6Responses => {
+            const orderDetails = select6Responses.map((response, index) => {
+                const order_id = orderIds[index];
+                const cartItems = response.data.List || []; // カートのアイテムがない場合は空配列
 
-                            return Promise.all(productPromises).then(itemsWithProductInfo => {
-                                return {
-                                    order_id,
-                                    total_quantity: totalQuantity,
-                                    items: itemsWithProductInfo
-                                };
-                            });
-                        });
-
-                        return Promise.all(orderDetails);
-                    });
-                })
-                .then(orderHistory => {
-                    this.orderHistory = orderHistory;
-                    console.log('Order History:', this.orderHistory);
-                })
-                .catch(error => {
-                    console.error('注文履歴の取得に失敗しました:', error);
+                let totalQuantity = 0;
+                cartItems.forEach(item => {
+                    totalQuantity += item.quantity;
                 });
-        },
+
+                // 各カートアイテムに対応する商品情報を取得する
+                const productPromises = cartItems.map(item =>
+                    axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT3', {
+                        params: { product_id: item.product_id }
+                    }).then(productResponse => {
+                        const productData = productResponse.data.List[0]; // 商品情報の1件目を使用
+                        return {
+                            ...item,
+                            product_name: productData.product_name,
+                            product_category: productData.product_category,
+                            product_gender: productData.product_gender,
+                            product_image_url: productData.URL
+                        };
+                    })
+                );
+
+                return Promise.all(productPromises).then(itemsWithProductInfo => {
+                    return {
+                        order_id,
+                        total_quantity: totalQuantity,
+                        items: itemsWithProductInfo
+                    };
+                });
+            });
+
+            return Promise.all(orderDetails);
+        });
+    })
+    .then(orderHistory => {
+        this.orderHistory = orderHistory;
+        console.log('Order History:', this.orderHistory);
+    })
+    .catch(error => {
+        console.error('注文履歴の取得に失敗しました:', error);
+    });
+},
         
         Logout() {
             window.location.href = '/index.html';
