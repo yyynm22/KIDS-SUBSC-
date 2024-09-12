b4447440a7e0e67eedc2b528cec794ee66b78206
aoiwhite7 <afurugoori@dl-techx-xedge.net>
2024-09-12 12:11:42 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -34,27 +34,54 @@ new Vue({
             window.location.href = '/index1.html';
         },
         fetchOrderHistory() {
+            // SELECT6の呼び出し
             axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT6')
-                .then(response => {
-                    console.log('API Response:', response.data);
-                    if (response.data && Array.isArray(response.data)) {
-                        this.orderHistory = response.data.map(order => ({
-                            order_id: order.order_id,
-                            total_quantity: order.total_quantity,
-                            items: order.items.map(item => ({
-                                product_id: item.product_id,
-                                product_name: item.product_name,
-                                product_category: item.product_category,
-                                product_size: item.product_size,
-                                product_gender: item.product_gender,
-                                quantity: item.quantity,
-                                product_image_url: item.URL
-                            }))
-                        }));
-                        console.log('Order History:', this.orderHistory);
-                    } else {
-                        console.error('期待する形式のデータが返されていません:', response.data);
-                    }
+                .then(select6Response => {
+                    const orderList = select6Response.data.List;
+                    const orderIds = orderList.map(order => order.order_id);
+
+                    // 各order_idに対してSELECT4を呼び出す
+                    const select4Promises = orderIds.map(order_id =>
+                        axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT4', {
+                            params: { order_id }
+                        })
+                    );
+
+                    return Promise.all(select4Promises);
+                })
+                .then(select4Responses => {
+                    const orderDetails = [];
+
+                    select4Responses.forEach((response, index) => {
+                        const order_id = response.config.params.order_id;
+                        const cartItems = response.data.List;
+
+                        // 各商品ごとにSELECT3を呼び出して商品情報を取得
+                        const productPromises = cartItems.map(item =>
+                            axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT3', {
+                                params: { product_id: item.product_id }
+                            })
+                        );
+
+                        orderDetails.push({
+                            order_id,
+                            cartItems
+                        });
+
+                        return Promise.all(productPromises).then(productResponses => {
+                            productResponses.forEach((productResponse, index) => {
+                                const productInfo = productResponse.data;
+                                orderDetails[index].cartItems[index].productInfo = productInfo;
+                            });
+                        });
+                    });
+
+                    return orderDetails;
+                })
+                .then(orderHistory => {
+                    // フロントエンドで取得したデータを使用して注文履歴を更新
+                    this.orderHistory = orderHistory;
+                    console.log('Order History:', this.orderHistory);
                 })
                 .catch(error => {
                     console.error('注文履歴の取得に失敗しました:', error);
