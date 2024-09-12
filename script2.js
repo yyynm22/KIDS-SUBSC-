b1a0ba185229bde3ce60720312381b9ad99430e7
aoiwhite7 <afurugoori@dl-techx-xedge.net>
2024-09-12 14:39:45 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -24,7 +24,7 @@ new Vue({
             this.userData.user_postcode = sessionStorage.getItem('user_postcode') || '';
             this.userData.user_adress = sessionStorage.getItem('user_adress') || '';
             this.userData.user_telenum = sessionStorage.getItem('user_telenum') || '';
-            this.userData.user_id = sessionStorage.getItem('user_mail') || '';
+            this.userData.user_id = sessionStorage.getItem('user_id') || ''; // 修正
             console.log('User Data:', this.userData);
         },
         togglePasswordVisibility() {
@@ -33,82 +33,69 @@ new Vue({
         addData() {
             window.location.href = '/index1.html';
         },
-        methods: {
-            fetchUserData() {
-                this.userData.user_name = sessionStorage.getItem('user_name') || '';
-                this.userData.user_pass = sessionStorage.getItem('user_pass') || '';
-                this.userData.user_mail = sessionStorage.getItem('user_mail') || '';
-                this.userData.user_postcode = sessionStorage.getItem('user_postcode') || '';
-                this.userData.user_adress = sessionStorage.getItem('user_adress') || '';
-                this.userData.user_telenum = sessionStorage.getItem('user_telenum') || '';
-                this.userData.user_id = sessionStorage.getItem('user_id') || ''; // 修正
-                console.log('User Data:', this.userData);
-            },
-            fetchOrderHistory() {
-                const userId = this.userData.user_id; // 現在のユーザーIDを取得
-        
-                axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT6')
-                    .then(select6Response => {
-                        const orderList = select6Response.data.List;
-                        const orderIds = orderList
-                            .filter(order => order.user_id === userId) // ユーザーIDでフィルタリング
-                            .map(order => order.order_id);
-            
-                        // 各order_idに対してSELECT4を呼び出す
-                        const select4Promises = orderIds.map(order_id =>
-                            axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT4', {
-                                params: { order_id }
-                            })
-                        );
-            
-                        return Promise.all(select4Promises).then(select4Responses => {
-                            const orderDetails = select4Responses.map((response, index) => {
-                                const order_id = orderIds[index];
-                                const cartItems = response.data.List || []; // カートのアイテムがない場合は空配列
-            
-                                let totalQuantity = 0;
-                                cartItems.forEach(item => {
-                                    totalQuantity += item.quantity;
-                                });
-            
-                                // 各カートアイテムに対応する商品情報を取得する
-                                const productPromises = cartItems.map(item =>
-                                    axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT3', {
-                                        params: { product_id: item.product_id }
-                                    }).then(productResponse => {
-                                        const productData = productResponse.data.List[0]; // 商品情報の1件目を使用
-                                        return {
-                                            ...item,
-                                            product_name: productData.product_name,
-                                            product_category: productData.product_category,
-                                            product_gender: productData.product_gender,
-                                            product_image_url: productData.URL
-                                        };
-                                    })
-                                );
-            
-                                return Promise.all(productPromises).then(itemsWithProductInfo => {
-                                    return {
-                                        order_id,
-                                        total_quantity: totalQuantity,
-                                        items: itemsWithProductInfo
-                                    };
-                                });
+        fetchOrderHistory() {
+            const userId = this.userData.user_id; // 現在のユーザーIDを取得
+
+            axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT6')
+                .then(select6Response => {
+                    const orderList = select6Response.data.List;
+                    const orderIds = orderList
+                        .filter(order => order.user_id === userId) // ユーザーIDでフィルタリング
+                        .map(order => order.order_id);
+
+                    // 各order_idに対してSELECT4を呼び出す
+                    const select4Promises = orderIds.map(order_id =>
+                        axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT4', {
+                            params: { order_id }
+                        })
+                    );
+
+                    return Promise.all(select4Promises).then(select4Responses => {
+                        const orderDetails = select4Responses.map((response, index) => {
+                            const order_id = orderIds[index];
+                            const cartItems = response.data.List || []; // カートのアイテムがない場合は空配列
+
+                            let totalQuantity = 0;
+                            cartItems.forEach(item => {
+                                totalQuantity += item.quantity;
                             });
-            
-                            return Promise.all(orderDetails);
+
+                            // 各カートアイテムに対応する商品情報を取得する
+                            const productPromises = cartItems.map(item =>
+                                axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT3', {
+                                    params: { product_id: item.product_id }
+                                }).then(productResponse => {
+                                    const productData = productResponse.data.List[0]; // 商品情報の1件目を使用
+                                    return {
+                                        ...item,
+                                        product_name: productData.product_name,
+                                        product_category: productData.product_category,
+                                        product_gender: productData.product_gender,
+                                        product_image_url: productData.URL
+                                    };
+                                })
+                            );
+
+                            return Promise.all(productPromises).then(itemsWithProductInfo => {
+                                return {
+                                    order_id,
+                                    total_quantity: totalQuantity,
+                                    items: itemsWithProductInfo
+                                };
+                            });
                         });
-                    })
-                    .then(orderHistory => {
-                        this.orderHistory = orderHistory;
-                        console.log('Order History:', this.orderHistory);
-                    })
-                    .catch(error => {
-                        console.error('注文履歴の取得に失敗しました:', error);
+
+                        return Promise.all(orderDetails);
                     });
-            },
+                })
+                .then(orderHistory => {
+                    this.orderHistory = orderHistory;
+                    console.log('Order History:', this.orderHistory);
+                })
+                .catch(error => {
+                    console.error('注文履歴の取得に失敗しました:', error);
+                });
         },
-        
         Logout() {
             window.location.href = '/index.html';
         }
